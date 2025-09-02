4bf4d3da104e7e43ec6decece75a878de6acbb8c
zcodlab <129913832+zcodlab@users.noreply.github.com>
2025-09-02 13:07:10 -0500

    252.uni.fp.s2a.SortSearch

---
+++ 252.uni.fp.s2a.SortSearch/sort.h
@@ -0,0 +1,16 @@
+#ifndef SORT_H
+#define SORT_H
+//Declaracion de la funcion Selection Sort
+void selectionSort(int X[], int n);
+
+//Declaracion de la funcion Bubble Sort
+void bubbleSort(int X[], int n);
+
+//Declaracion de la funcion Insertion Sort
+void insertionSort(int X[], int n);
+
+//Declaracion de la funcion Insertion Binary Sort
+void insertionBinarySort(int X[], int n);
+
+
+#endif // SORT_H
