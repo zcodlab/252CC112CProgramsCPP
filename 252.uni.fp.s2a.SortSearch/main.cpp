2236694556b02339d2e0db8f9129d7e8e875fdb2
zcodlab <129913832+zcodlab@users.noreply.github.com>
2025-09-02 13:23:13 -0500

    agregando logica parcial a la funcion main

--- 252.uni.fp.s2a.SortSearch/main.cpp
+++ 252.uni.fp.s2a.SortSearch/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
-
+#include "sort.h"
 using namespace std;
+
+const int MAX=100; //tamaño maximo permitido para el arreglo
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int X[MAX], C[MAX];     //arrays estaticos, C se utilizara como copia de X con la finalidad de matener los datos originales y poder someter a prueba los diferentes algoritmos de ordenamiento
+    int n=0;
+    cout << "Ingrese el tamaño del array (max: "<<MAX<<" ): ";
+    cin>>n;
+    //validar el valor ingresado
+    if(n>MAX || n<=0){
+            cout<<"Tamaño ingresado es invalido."<<endl;
+            return 0;
+    }
+    //Invocar al menu de operaciones
+    menu(X,C,n);
     return 0;
 }
