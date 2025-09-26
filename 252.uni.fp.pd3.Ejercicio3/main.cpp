b60d41255404dd8a72fb7f8e188c08d4d103e933
zcodlab <129913832+zcodlab@users.noreply.github.com>
2025-09-26 12:58:51 -0500

    252.uni.fp.pd3.Ejercicio3

---
+++ 252.uni.fp.pd3.Ejercicio3/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+unsigned int strlen(const char* p){
+    int i=0;
+    while(*(p+i)!='\0')
+        i++;
+    return i;
+}
+
+void rev(char* a){
+    int n=strlen(a);
+    for(int i=0; i<n/2;i++)
+        swap(*(a+i),*(a+n-i-1));
+}
+
+int main()
+{
+    char a[10]={};
+    cout << "Ingrese la palabra:" << endl;
+    cin>>a;
+    rev(a);
+    //imprimir la palabra invertida
+    for(auto i : a) //auto infiere que i es un char
+            cout << i;
+    return 0;
+}
